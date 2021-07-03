081caac667500345b7d0b81c2f5ee41e5f6eead8
Una Ada <trewbot@gmail.com>
2021-07-03 14:54:02 -0700

    feat(ctrl): add users controller

---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+/**
+ * Users controller
+ * @author Una Ada <una@anarchy.website>
+ * @version 2021.07.03
+ * @module controllers/users
+ * @see module:routes/users
+ */
+
+/*----- Imports --------------------------------------------------------------*/
+import Jikan from 'jikan-node';
+const mal = new Jikan();
+
+/*----- Export Modules -------------------------------------------------------*/
+export default {
+  
+}
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,12 @@
  * @author Una Ada <una@anarchy.website>
  * @version 2021.07.03
  * @module routes/users
+ * @see module:controllers/users
  */
 
 /*----- Imports --------------------------------------------------------------*/
 import { Router } from 'express';
-// import usersCtrl from '../controllers/users.js';
+import usersCtrl from '../controllers/users.js';
 
 /*----- Routes ---------------------------------------------------------------*/
 const router = new Router();
