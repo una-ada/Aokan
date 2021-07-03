61a8873cc394cd66540fae3c709ace0d452c244b
Una Ada <trewbot@gmail.com>
2021-07-03 14:51:39 -0700

    feat(route): adds users router

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+/**
+ * User related routes
+ * @author Una Ada <una@anarchy.website>
+ * @version 2021.07.03
+ * @module routes/users
+ */
+
+/*----- Imports --------------------------------------------------------------*/
+import { Router } from 'express';
+// import usersCtrl from '../controllers/users.js';
+
+/*----- Routes ---------------------------------------------------------------*/
+const router = new Router();
+
+/*----- Exports --------------------------------------------------------------*/
+export default router;
