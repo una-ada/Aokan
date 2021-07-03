891608cc3cc487bca708497eda01c99d1c8fdc43
Una Ada <trewbot@gmail.com>
2021-07-03 01:18:18 -0700

    feat(view): add responders

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,29 @@
 /**
  * Aokan client main module.
  * @author Una Ada <una@anarchy.website>
- * @version 2021.06.27
+ * @version 2021.07.03
  */
 
-console.log('yay this is working at least');
+document.addEventListener('click', e =>
+  (res =>
+    res &&
+    Object.assign(res.style, {
+      transition: '',
+      opacity: 1,
+      webkitClipPath: `circle(0px at ${e.offsetX}px ${e.offsetY}px)`,
+    }) &&
+    requestAnimationFrame(_ =>
+      Object.assign(res.style, {
+        transitionDuration: '1s',
+        opacity: 0,
+        webkitClipPath: `circle(${(rect =>
+          (rect.width ** 2 + rect.height ** 2) ** 0.5)(
+          res.getBoundingClientRect()
+        )}px at ${e.offsetX}px ${e.offsetY}px)`,
+      })
+    ))(
+    e.path
+      .find(el => el.classList && el.classList.contains('text-field'))
+      ?.querySelector('.text-field__responder')
+  )
+);
