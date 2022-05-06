cfe0b7a481911644e586389ab7637dd157ae05dc
Una Hoglund <trewbot@gmail.com>
2022-05-06 02:28:15 -0700

    fix: change name from Aokan-2 to Aokan

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/aokan-2'),
+      dir: require('path').join(__dirname, './coverage/aokan'),
       subdir: '.',
       reporters: [
         { type: 'html' },
