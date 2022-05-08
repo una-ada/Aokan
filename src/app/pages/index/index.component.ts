58d543b4dc3127dcfd61048c21dc5b201b9a57a7
Una Hoglund <trewbot@gmail.com>
2022-05-08 06:36:30 -0700

    docs: update version dates

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,21 @@
+/**
+ * Index page component
+ * @author Una Ada <una@xn--z7x.dev>
+ * @version 2022.05.08
+ */
+
+/*----- Imports --------------------------------------------------------------*/
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+/*----- Component ------------------------------------------------------------*/
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
+
+/*----- Exports --------------------------------------------------------------*/
 export class IndexComponent implements OnInit {
   isLinear = true;
   characterSelect!: FormGroup;
